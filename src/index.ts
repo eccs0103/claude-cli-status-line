0f08b37b8d1406271778d75cd106ff88be8be97e
Arman <eccs0103@gmail.com>
2026-06-11 04:19:19 +0400

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 "use strict";
 
 import "adaptive-extender/node";
-import { ConfigurationController } from "./controllers/config-controller.js";
+import { ConfigurationController } from "./controllers/configuration-controller.js";
 import { StatusLineController } from "./controllers/status-line-controller.js";
 
-const { argv } = process;
-const [, , section] = argv;
-if (section === "config") await ConfigurationController.launch();
-else await StatusLineController.launch();
+const [, , section] = process.argv;
+switch (section) {
+case "config": await ConfigurationController.launch(); break;
+default: await StatusLineController.launch(); break;
+}
